4fe7fcec7bc2127d47b1e5c21dba3151cc896d0c
Zane van Iperen <zane@zanevaniperen.com>
2021-08-27 18:57:53 +1000

    plasma5Packages.breeze-qt5: backport fix for https://bugs.kde.org/show_bug.cgi?id=436473

--- pkgs/desktops/plasma-5/breeze-qt5.nix
+++ pkgs/desktops/plasma-5/breeze-qt5.nix
@@ -1,6 +1,6 @@
 {
   mkDerivation,
-  extra-cmake-modules,
+  fetchpatch, extra-cmake-modules,
   frameworkintegration, kcmutils, kconfigwidgets, kcoreaddons, kdecoration,
   kguiaddons, ki18n, kwayland, kwindowsystem, plasma-framework, qtdeclarative,
   qtx11extras, fftw
@@ -17,4 +17,12 @@ mkDerivation {
   ];
   outputs = [ "bin" "dev" "out" ];
   cmakeFlags = [ "-DUSE_Qt4=OFF" ];
+
+  patches = [
+    # https://bugs.kde.org/show_bug.cgi?id=436473
+    (fetchpatch {
+      url = "https://invent.kde.org/plasma/breeze/-/commit/f99b7ef621c9c69544158d245699fd8104db6753.patch";
+      sha256 = "1dd4qa56md39w9nz3vih2dqpbycvm9cvd4iqw7zi7qh64jzxhb47";
+    })
+  ];
 }
